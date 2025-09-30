9562ac8d392112869671aca3373cbcf4bb9e8e5e
Ali Hasanov <nan.fgbs@gmail.com>
2025-09-30 11:26:56 +0400

    fix: Fix  UI compomponent & add Back button in ProductDetails

--- src/pages/ProductDetailsPage.jsx
+++ src/pages/ProductDetailsPage.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
-import { useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 import { products } from '../data/products';
 import Product from '../components/Product';
 
 function ProductDetailsPage() {
     const {name} = useParams(); 
+    const navigate =  useNavigate();
   return (
     <div className='main_section'>
       <h2 className='mt-16'>Product Details:</h2>
-      <div>
+      <div className='mt-10 flex flex-col justify-center items-center  gap-6'>
         {
             products && products.map((product)=>{
                 if(product.name == name){
@@ -16,6 +17,7 @@ function ProductDetailsPage() {
                 }
             })
         }
+        <button className='px-4 py-2 text-center bg-green_3 rounded-lg text-lg' onClick={()=>navigate('/products')}>Back</button>
       </div>
     </div>
   )
